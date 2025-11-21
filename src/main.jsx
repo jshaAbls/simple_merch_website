b4905d3e395ae481a1f8e0352e1dfdf50e6c58c0
jshaAbls <joshua0913abalos@gmail.com>
2025-11-21 17:30:34 +0800

    router use change at main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
  import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
+import { createRoot } from 'react-dom/client'
+// import { BrowserRouter } from 'react-router-dom'
+
+import { HashRouter } from "react-router-dom";
 
 import App from './App.jsx'
 
@@ -11,8 +13,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
+<HashRouter>
+  <App />
+</HashRouter>
   </StrictMode>
 )
