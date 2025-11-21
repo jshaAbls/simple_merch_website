6c9da30d83a79a7211db548689c8ea3c53f05ab6
jshaAbls <joshua0913abalos@gmail.com>
2025-11-21 18:57:25 +0800

    Modified router lines

--- src/main.jsx
+++ src/main.jsx
@@ -13,8 +13,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-<HashRouter>
-  <App />
-</HashRouter>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 )
